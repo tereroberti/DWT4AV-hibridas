bc957d41ca63fa3d9e37d71e0370900ca6e66b9a
tereroberti <146693747+tereroberti@users.noreply.github.com>
2025-08-21 16:19:59 -0300

    nuevo cambio

--- clase1/index.js
+++ clase1/index.js
@@ -1,1 +1,7 @@
-console.log("hola!")
+const http = require("http")
+//import http from "http"
+
+http.createServer(function(request, response){
+console.log("hola desde el servidor!", request.url )
+response.end("llego la solicitud")
+}).listen(2025)
