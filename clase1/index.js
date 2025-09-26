3e0bf13d4bff1f715b27ad1be54cb17bf5439cc0
tereroberti <146693747+tereroberti@users.noreply.github.com>
2025-09-26 03:32:31 -0300

    Delete clase1 directory

--- clase1/index.js
+++
@@ -1,7 +0,0 @@
-const http = require("http")
-//import http from "http"
-
-http.createServer(function(request, response){
-console.log("hola desde el servidor!", request.url )
-response.end("llego la solicitud")
-}).listen(2025)
